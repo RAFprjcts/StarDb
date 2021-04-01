0c848a246525eff0c4a0ae3ffdcf1d9b4acda93a
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-01 16:07:42 +0300

    arrow function

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -12,7 +12,7 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResource(`/people/`);
-    return res.results;
+    return res.results.map(this._transformPerson);
   }
 
   async getPerson(id) {
@@ -32,7 +32,7 @@ export default class SwapiService {
 
   async getAllStarships() {
     const res = await this.getResource(`/starships/`);
-    return res.results;
+    return res.results.map(this._transformStarship);
   }
 
   async getStarship(id) {
@@ -45,7 +45,7 @@ export default class SwapiService {
     return item.url.match(idRegExp)[1];
   }
 
-  _transformPlanet(planet) {
+  _transformPlanet = (planet) => {
     return {
       id: this._extractId(planet),
       name: planet.name,
@@ -53,9 +53,9 @@ export default class SwapiService {
       rotationPeriod: planet.rotation_period,
       diameter: planet.diameter,
     };
-  }
+  };
 
-  _transformStarship(starship) {
+  _transformStarship = (starship) => {
     return {
       id: this._extractId(starship),
       name: starship.name,
@@ -67,9 +67,9 @@ export default class SwapiService {
       passengers: starship.passengers,
       cargoCapacity: starship.cargoCapacity,
     };
-  }
+  };
 
-  _transformPerson(person) {
+  _transformPerson = (person) => {
     return {
       id: this._extractId(person),
       name: person.name,
@@ -77,5 +77,5 @@ export default class SwapiService {
       birthYear: person.birthYear,
       eyeColor: person.eyeColor,
     };
-  }
+  };
 }
