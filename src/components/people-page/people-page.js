9f3ca796a1dfaab400c17fa27c541024d869c0f5
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-03 19:54:48 +0300

    Edit: refactiroing, use children props

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import SwapiService from "../../services/swapi-service";
-import ErrorIndicator from "../error-indicator";
+import ErrorBoundry from "../error-boundry";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
+import Row from "../row";
 
 import "./people-page.css";
 
@@ -11,39 +12,27 @@ export default class PeoplePage extends Component {
 
   state = {
     selectedPerson: 3,
-    hasError: false,
   };
-
-  componentDidCatch(error, info) {
-    this.setState({
-      hasError: true,
-    });
-  }
 
   onPersonSelected = (selectedPerson) => {
     this.setState({ selectedPerson });
   };
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
-
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({ name, gender, birthYear }) =>
-              `${name} (${gender}, ${birthYear})`
-            }
-          />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+      >
+        {(i) => `${i.name} (${i.birthYear})`}
+      </ItemList>
     );
+
+    const personDetails = (
+      <ErrorBoundry>
+        <PersonDetails personId={this.state.selectedPerson} />
+      </ErrorBoundry>
+    );
+    return <Row left={itemList} right={personDetails} />;
   }
 }
