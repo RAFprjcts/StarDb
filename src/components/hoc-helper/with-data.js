e5cf1f6d19f15d65ad871f6716c4e76232d9f0a3
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-04 20:35:26 +0300

    refactoring add hoc

---
+++ src/components/hoc-helper/with-data.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Spinner from "../spinner";
+
+const withData = (View, getData) => {
+  return class extends Component {
+    state = {
+      data: null,
+    };
+
+    componentDidMount() {
+      getData().then((data) => {
+        this.setState({
+          data,
+        });
+      });
+    }
+
+    render() {
+      const { data } = this.state;
+
+      if (!data) {
+        return <Spinner />;
+      }
+
+      return <View data={data} {...this.props} />;
+    }
+  };
+};
+
+export default withData;
