d076d341c9aeacb7fba1e711bcba1308a988983f
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-08 17:38:04 +0300

    add and use new pages component

---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+import PeoplePage from "./people-page";
+import PlanetsPage from "./planets-page";
+import StarshipsPage from "./starhips-page";
+
+export { PeoplePage, PlanetsPage, StarshipsPage };
